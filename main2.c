9e392de3f55879be6a18ef7babe31ff53c5bd914
Marcio Vennan <marciovenann@gmail.com>
2015-11-18 00:03:58 -0300

    limpando codigo e comentando

--- main2.c
+++ main2.c
@@ -10,10 +10,8 @@
 int main(int argc,char** argv)
 {
    MPI_Status status;
-   int     myrank;
-   int     position;
-   int     n;
-   float   a, b;
+   int     rank;
+   int     posicao;
    char    buffer[TAM * 4];
    int vet[TAM];
    int *vet1, *vet2, *vet3;
@@ -21,111 +19,82 @@ int main(int argc,char** argv)
    int tag;
  
    MPI_Init(&argc, &argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int i;
-   if (myrank == 0){
-      n = 4;
-      a = 1.;
-      b = 2.;
-      printf("Rank=%d, a= %f, b= %f, and n=%d\n", myrank, a, b, n);
-      position = 0;
+   if (rank == 0){
 
+      posicao = 0;
+
+      // Preeche vetor com números aleatórios
       preencheVetor(vet, TAM);
 
-
-
+      // Separa um vetor em dois e retorna um pivô. 
       pivo = separa(vet, 0, TAM -1);
 
-        
-
-      // vet1 = (int *)malloc(sizeof(int) * pivo);
-      // memcpy(vet1, vet, pivo * sizeof(int));
-
-      
-
-      MPI_Pack(vet, TAM, MPI_INT, buffer, TAM * 4, &position, MPI_COMM_WORLD);
-      //MPI_Pack(&pivo, 1, MPI_INT, buffer, TAM, &position, MPI_COMM_WORLD);
-      //MPI_Pack(&n, 1, MPI_INT, buffer, TAM, &position, MPI_COMM_WORLD);
+      // Empacgota o vetor para ser enviados aos processos
+      MPI_Pack(vet, TAM, MPI_INT, buffer, TAM * 4, &posicao, MPI_COMM_WORLD);
  
-    // communication
+    // Envia o vetor para os outros processos
      MPI_Bcast(buffer, TAM * 4, MPI_PACKED, 0, MPI_COMM_WORLD);
+
+     // Envia o pivê para os outros processos
      MPI_Bcast(&pivo, 1, MPI_INT, 0, MPI_COMM_WORLD); 
      //MPI_Send(buffer, TAM * 4, MPI_PACKED, 1, tag, MPI_COMM_WORLD);
  
    }else{
-    // communication
-    MPI_Bcast(buffer, TAM * 4, MPI_PACKED, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&pivo, 1, MPI_INT, 0, MPI_COMM_WORLD); 
-      //MPI_Bcast(buffer, 100, MPI_PACKED, 0, MPI_COMM_WORLD);
-      position = 0;
-      //printf("pivo = %d \n", pivo);
+      // 
+      MPI_Bcast(buffer, TAM * 4, MPI_PACKED, 0, MPI_COMM_WORLD);
+      MPI_Bcast(&pivo, 1, MPI_INT, 0, MPI_COMM_WORLD); 
+        //MPI_Bcast(buffer, 100, MPI_PACKED, 0, MPI_COMM_WORLD);
+      posicao = 0;
+    
+      //printf("Pivo = %d\n", pivo );
 
-      //MPI_Unpack(buffer, TAM, &position, &pivo, 1, MPI_FLOAT, MPI_COMM_WORLD);
+      // Desempacota o vetor
+      MPI_Unpack(buffer, TAM * 4, &posicao, vet, TAM, MPI_INT, MPI_COMM_WORLD);
 
-      //vet1 = (int *)malloc(sizeof(int) * 10);
- printf("Pivo = %d\n", pivo );
-    // unpacking
-      //int vet2[5] = {1, 2, 3, 4 ,5};
-      //MPI_Recv(buffer, TAM * 4, MPI_PACKED, 0, tag, MPI_COMM_WORLD, &status);
+      // Se processo 1, ordena os valores do lado esquerdo do pivô
+      if (rank == 1){
 
-      MPI_Unpack(buffer, TAM * 4, &position, vet, TAM, MPI_INT, MPI_COMM_WORLD);
-
-      if (myrank == 1){
           ordenacao(vet, 0, pivo);
+
+          // Envia os valores ordenados para o processo 0
           MPI_Send(&vet, TAM, MPI_INT, 0, tag, MPI_COMM_WORLD);
-      }else if(myrank == 2){
+      }else if(rank == 2){// Se processo 2, ordena os valores do lado direto do pivô
+          
           ordenacao(vet, pivo + 1, TAM);
+
+          // Envia os valores ordenados para o processo 0
           MPI_Send(&vet, TAM, MPI_INT, 0, tag, MPI_COMM_WORLD);
       }
       
-
-      
-
-
-      //int i;
-      //for ( i = 0; i < 10; ++i)
-        // printf("%d - ", vet[0]);
-        //         printf("%d - ", vet[1]);
-
-        // printf("%d - ", vet[2]);
-
-        // printf("%d - ", vet[3]);
-
-        // printf("%d - ", vet[4]);
-     
-
-      //{
-      //}
-      //printf("\n");
-      //MPI_Unpack(buffer, 100, &position, &b, 1, MPI_FLOAT, MPI_COMM_WORLD);
-      //MPI_Unpack(buffer, 100, &position, &n, 1, MPI_INT, MPI_COMM_WORLD);
-      //printf("Rank=%d, a= %d \n", myrank, vet1[1]);
    }
- //MPI_Barrier(MPI_COMM_WORLD);
+;
 
-   if (myrank == 0){
-     vet1 = (int *)malloc(sizeof(int) * pivo);
-     vet2 = (int *)malloc(sizeof(int) * (TAM - pivo));
-     vet3 = (int *)malloc(sizeof(int) * TAM);
+   if (rank == 0){
 
-     memcpy(vet3, vet1, pivo * sizeof(int));
-     memcpy(vet3 + pivo, vet2, (TAM - pivo) * sizeof(int));
+     //Alocando novos vetores que armazenarão os números ordenados
+     // vet1 = (int *)malloc(sizeof(int) * pivo);
+     // vet2 = (int *)malloc(sizeof(int) * (TAM - pivo));
+     // vet3 = (int *)malloc(sizeof(int) * TAM);
+
+     // memcpy(vet3, vet1, pivo * sizeof(int));
+     // memcpy(vet3 + pivo, vet2, (TAM - pivo) * sizeof(int));
 
     //printf("Pivo = %d\n", pivo );
-      MPI_Recv(vet1, TAM, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
-      MPI_Recv(vet2, TAM, MPI_INT, 2, tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(vet, TAM, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(vet, TAM, MPI_INT, 2, tag, MPI_COMM_WORLD, &status);
 
-      for(i = 0; i < TAM; i++){
-        printf("%d - ", vet3[i]);
-      }
-      printf("\n\n\n");
+      // for(i = 0; i < pivo; i++){
+      //   printf("%d - ", vet[i]);
+      // }
+      // printf("\n\n\n");
 
       // for(i = pivo + 1; i < TAM; i++){
-      //   printf("%d - ", vet2[i]);
+      //   printf("%d - ", vet[i]);
       // }
       // printf("\n");
    }
-   free(vet1);
-   free(vet2);
+
    MPI_Finalize();
 }
